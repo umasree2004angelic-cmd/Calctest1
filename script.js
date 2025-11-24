8c4bc427fca41664b7cafc83ecb0c77c0957c404
umasree2004angelic-cmd <umasree2004angelic@gmail.com>
2025-11-24 12:26:12 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+const resultInput = document.getElementById('result');
+
+function appendValue(value) {
+    resultInput.value += value;
+}
+
+function clearResult() {
+    resultInput.value = '';
+}
+
+function deleteLast() {
+    resultInput.value = resultInput.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        resultInput.value = eval(resultInput.value);
+    } catch (error) {
+        resultInput.value = 'Error';
+    }
+}
